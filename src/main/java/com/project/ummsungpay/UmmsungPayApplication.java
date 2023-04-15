07d270ce8aa85687bf7c392af9a89b265cf13ca9
Eugene <eugenebori@naver.com>
2023-04-15 18:44:42 +0900

    Initial commit

---
+++ src/main/java/com/project/ummsungpay/UmmsungPayApplication.java
@@ -0,0 +1,13 @@
+package com.project.ummsungpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmmsungPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UmmsungPayApplication.class, args);
+    }
+
+}
