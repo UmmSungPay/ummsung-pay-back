a122fb3da0b9c5d7a5191d8064d98f35c5b88ed1
Eugene <eugenebori@naver.com>
2023-07-24 21:07:56 +0900

    [FIX] 구글로 로그인 완료

---
+++ src/main/java/com/project/ummsungpay/domain/user/Role.java
@@ -0,0 +1,15 @@
+package com.project.ummsungpay.domain.user;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+
+//사용자의 권한을 관리할 Enum 클래스 Role
+public enum Role {
+    GUEST("ROLE_GUEST", "손님"),
+    USER("ROLE_USER", "일반 사용자");
+
+    private final String key;
+    private final String title;
+}
