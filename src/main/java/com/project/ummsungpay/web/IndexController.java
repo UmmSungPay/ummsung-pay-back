d765d1b6df4c83ed2333fb919d9b997350cbb2cd
Eugene <eugenebori@naver.com>
2023-07-28 01:23:23 +0900

    [ADD] 로그아웃

--- src/main/java/com/project/ummsungpay/web/IndexController.java
+++ src/main/java/com/project/ummsungpay/web/IndexController.java
@@ -1,12 +1,25 @@
 package com.project.ummsungpay.web;
 
+import com.project.ummsungpay.config.auth.dto.SessionUser;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import javax.servlet.http.HttpSession;
 
+
+
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
+    private final HttpSession httpSession;
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 }
